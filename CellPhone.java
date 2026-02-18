d992acd03492ee3037ad264e68d99659dd28884e
Incharagowda55 <vanivanisuresh490@gmail.com>
2026-02-18 09:14:55 +0530

    CellPhone

---
+++ CellPhone.java
@@ -0,0 +1,201 @@
+class CellPhone{
+	public static long getContactNumber(String ContactName){
+		long ContactNo=0L;
+		if(ContactName=="Arya"){
+			ContactNo=8217077806L;
+			return ContactNo;
+		}
+		else if(ContactName=="Kishan"){
+			ContactNo=234567891L;
+			return ContactNo;
+		}
+		else if(ContactName=="Chethu"){
+			ContactNo=9956789101L;
+			return ContactNo;
+		}
+		else if(ContactName=="Jeevan"){
+			ContactNo=9867891012L;
+			return ContactNo;
+		}
+		else if(ContactName=="Gani "){
+			ContactNo=8889101234L;
+			return ContactNo;
+		}
+		else if(ContactName=="Giri"){
+			ContactNo=8789101245L;
+			return ContactNo;
+		}
+		else if(ContactName=="Hiriyanna"){
+			ContactNo=8910123456L;
+			return ContactNo;
+		}
+		else if(ContactName=="inchara"){
+			ContactNo=8910123456L;
+			return ContactNo;
+		}
+		else if(ContactName=="Kitty"){
+			ContactNo=9101112345L;
+			return ContactNo;
+		}
+		else if(ContactName=="Leo"){
+			ContactNo=9934567892L;
+			return ContactNo;
+		}
+		else if(ContactName=="Manish"){
+			ContactNo=20345678988L;
+			return ContactNo;
+		}
+		
+		else if(ContactName=="Suri"){
+			ContactNo=986756321L;
+			return ContactNo;
+		}
+		else if(ContactName=="Madan"){
+			ContactNo=986756321L;
+			return ContactNo;
+		}
+		else if(ContactName=="Ammu"){
+			ContactNo=926756321L;
+			return ContactNo;
+		}
+		else if(ContactName=="Jyo"){
+			ContactNo=886756321L;
+			return ContactNo;
+		}
+		else if(ContactName=="Likihitha"){
+			ContactNo=996756321L;
+			return ContactNo;
+		}
+		else if(ContactName=="BS"){
+			ContactNo=988756321L;
+			return ContactNo;
+		}
+		else if(ContactName=="Manji dodama"){
+			ContactNo=986756321L;
+			return ContactNo;
+		}
+		else if(ContactName=="Jyothi"){
+			ContactNo=7760012990L;
+			return ContactNo;
+		}
+		else if(ContactName=="Amma"){
+			ContactNo=9867563290L;
+			return ContactNo;
+		}
+		else if(ContactName=="Appa"){
+			ContactNo=97675632998L;
+			return ContactNo;
+		}
+		else if(ContactName=="Koli"){
+			ContactNo=9967567889L;
+			return ContactNo;
+		}
+		else if(ContactName=="Ajji"){
+			ContactNo=9867565521L;
+			return ContactNo;
+		}
+		else if(ContactName=="Adderi ajji "){
+			ContactNo=9867667831L;
+			return ContactNo;
+		}
+		else if(ContactName=="Thata"){
+			ContactNo=9267563218L;
+			return ContactNo;
+		}
+		else if(ContactName=="Anna"){
+			ContactNo=9767563219L;
+			return ContactNo;
+		}
+		else if(ContactName=="Lav"){
+			ContactNo=9867563288L;
+			return ContactNo;
+		}
+		else if(ContactName=="Vikas "){
+			ContactNo=9567563277L;
+			return ContactNo;
+		}
+		else if(ContactName=="Vinay"){
+			ContactNo=906756721L;
+			return ContactNo;
+		}
+		
+		else if(ContactName=="Akash Bilur"){
+			ContactNo=986756356L;
+			return ContactNo;
+		}
+		else if(ContactName=="Harsha"){
+			ContactNo=98675443276L;
+			return ContactNo;
+		}
+		else if(ContactName=="Ananya h raj"){
+			ContactNo=986756323L;
+			return ContactNo;
+		}
+		else if(ContactName=="Annappa"){
+			ContactNo=986756399L;
+			return ContactNo;
+		}
+		else if(ContactName=="Sandya akka"){
+			ContactNo=9867563284L;
+			return ContactNo;
+		}
+		else if(ContactName=="Anvitha "){
+			ContactNo=989956321L;
+			return ContactNo;
+		}
+		else if(ContactName=="Arjun Doc"){
+			ContactNo=9867987651L;
+			return ContactNo;
+		}
+		else if(ContactName=="Asok anna"){
+			ContactNo=9876543218L;
+			return ContactNo;
+		}
+		else if(ContactName=="Ayesha mam"){
+			ContactNo=9867785432L;
+			return ContactNo;
+		}
+		else if(ContactName=="Suri"){
+			ContactNo=986756321L;
+			return ContactNo;
+		}
+		else if(ContactName=="Badige Mane"){
+			ContactNo=9845198675L;
+			return ContactNo;
+		}
+		else if(ContactName=="Bhuvana"){
+			ContactNo=9867563776L;
+			return ContactNo;
+		}
+		else if(ContactName=="Bhuvan s shet"){
+			ContactNo=765432893L;
+			return ContactNo;
+		}
+		else if(ContactName=="Chethana"){
+			ContactNo=9876543219L;
+			return ContactNo;
+		}
+		else if(ContactName=="Deekshitha"){
+			ContactNo=8976567898L;
+			return ContactNo;
+		}
+		else if(ContactName=="disha"){
+			ContactNo=8217056780L;
+			return ContactNo;
+		}
+		else if(ContactName=="Doddapa"){
+			ContactNo=9845198196L;
+			return ContactNo;
+		}
+		
+
+		else{
+			System.out.println("Plz check the name");
+		}
+		
+		return ContactNo;
+	}
+}	
+		
+			
+		
